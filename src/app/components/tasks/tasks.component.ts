34de15898c14074db279763efd9da0ce97b07bc2
Vinicius de Souza Santos <69911251+gladistory@users.noreply.github.com>
2024-12-07 19:11:00 -0300

    Tela de task concluida

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field'; // MatFormFieldModule
 import { MatInputModule } from '@angular/material/input'; // MatInputModule
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-tasks',
@@ -14,7 +15,18 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  encapsulation: ViewEncapsulation.None, // Permite aplicar estilos globais
+  encapsulation: ViewEncapsulation.None, 
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class TasksComponent {
   showForm = false; // Controla a exibição do formulário
