6c87a618fa910ceffa69bea42fb641135162d31e
Vinicius de Souza Santos <desouza850@gmail.com>
2024-12-22 12:31:37 -0300

    front finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { LoginComponent } from './components/login/login.component';
 export const routes: Routes = [
     {path: 'tasks', component: TasksComponent},
-    {path: 'login', component: LoginComponent},
+    {path: '', component: TasksComponent},
 ];
