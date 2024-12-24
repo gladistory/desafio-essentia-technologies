4abc0cad2919aba6a27ede5f1ec1020d24b9f1df
Vinicius de Souza Santos <desouza850@gmail.com>
2024-12-24 11:18:12 -0300

    deleteTask funcionando

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,12 +2,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../../services/tasks.service';
+import { Task } from '../../../../Task';
 
 @Component({
   selector: 'app-tasks',
   imports: [
     CommonModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    FormsModule
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
@@ -16,10 +20,10 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('fadeInOut', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('500ms ease', style({ opacity: 1 }))
+        animate('700ms ease', style({ opacity: 1 }))
       ]),
       transition(':leave', [
-        animate('500ms ease', style({ opacity: 0 }))
+        animate('200ms ease', style({ opacity: 0 }))
       ])
     ])
   ]
@@ -27,7 +31,65 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export class TasksComponent {
   showForm = false; // Controla a exibição do formulário
 
+  tasks: Task[] = [];
+  newTask: Task = {title: '', description: '', time: '', status: ''}
+
+  constructor(private taskService: TasksService) {}
+
+  ngOnInit(): void {
+    this.getAllTasks();
+  }
+
   toggleForm() {
     this.showForm = !this.showForm; // Alterna entre mostrar/ocultar
   }
+
+  // Pega todas as tasks
+  getAllTasks(): void {
+    this.taskService.getTasks().subscribe({
+      next: (data) => (this.tasks = data),
+      error: (error) => console.error(error),
+      complete: () => console.log('Fetch complete'),
+    });
+  }
+
+  // Add a new task
+  addTask(): void {
+    if (!this.newTask.title || !this.newTask.description || !this.newTask.time || !this.newTask.status) {
+      alert('Por favor, preencha todos os campos');
+      return;
+    }
+
+    this.taskService.createTask(this.newTask).subscribe({
+      next: (data) => {
+        this.tasks.push(data);
+        this.newTask = { title: '', description: '', time: '', status: '' }; // Clear form
+      },
+      error: (error) => console.error(error),
+      complete: () => console.log('task creation completed'),
+    });
+    
+  }
+
+  // Deletar uma tarefa
+  deleteTask(id: number | undefined): void {
+    if (id === undefined) {
+      console.error('Task ID não definido');
+      return;
+    }
+  
+    if (confirm('Deseja realmente deletar essa tarefa?')) {
+      this.taskService.deleteTask(id).subscribe({
+        next: () => {
+          console.log(`Task ${id} deleted successfully`);
+          this.getAllTasks();
+        },
+        error: (error) => {
+          console.error('Error deleting task', error);
+        }
+      });
+    }
+  }
+  
+
 }
---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../../../Task';
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  private apiUrl = 'http://localhost:3001/api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  // Get all users
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+   // Create a new user
+   createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+   // Deletar uma tarefa
+   deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+
+}
